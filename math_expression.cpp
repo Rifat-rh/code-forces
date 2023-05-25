ebc02f019d533943051650f254d5ca44f903bf3c
Rifat-rh <129538138+Rifat-rh@users.noreply.github.com>
2023-05-25 13:33:54 +0600

    upload

---
+++ math_expression.cpp
@@ -0,0 +1,44 @@
+/*
+Question-1: 
+Given a mathematical expression. The expression will be one of the following expressions:
+A + B = C, A - B = C and A * B = C
+where A, B, C are three numbers, S is the sign between A and B, and Q the '=' sign
+Print "Yes" If the expression is Right , Otherwise print the right answer of the expression.
+
+Input:
+Only one line containing the expression: A, S, B, Q, C respectively (0 ≤ A, B ≤ 100,  - 105 ≤ C ≤ 105) and S can be ('+', '-', '*') without the quotation.
+
+Output:
+Output either "Yes" (without the quotation) or the right answer depending on the statement
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    char s,q;
+    cin >> a >> s >> b >> q >> c;
+
+    if( s == '+' ){
+        if( a+b == c ){
+            cout << "Yes\n";
+        }else{
+            cout << a+b << endl;
+        }
+    }else if( s == '-' ){
+        if( a-b == c ){
+            cout << "Yes\n";
+        }else{
+            cout << a-b << endl;
+        }
+    }else if( s == '*' ){
+        if( a*b == c ){
+            cout << "Yes\n";
+        }else{
+            cout << a*b << endl;
+        }
+    }
+    
+    return 0;
+}
