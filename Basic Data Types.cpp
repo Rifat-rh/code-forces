1d0d63e65338de1c20805050210db4c475c49fcf
Rifat-rh <129538138+Rifat-rh@users.noreply.github.com>
2023-06-02 17:02:43 +0600

    Add files via upload
    basic

---
+++ Basic Data Types.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    long long int a;
+    char x;
+    float z;
+     double c;
+      cin>>n>>a>>x>>z>>c;
+      cout<<n<<endl<<a<<endl<<x<<endl<<z<<endl<<c;
+
+    return 0;
+}
