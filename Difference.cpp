047196be0b93bc85b27fdf955c46d54680fd3c31
Rifat-rh <129538138+Rifat-rh@users.noreply.github.com>
2023-06-02 17:13:23 +0600

    Add files via upload
    difference

---
+++ Difference.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   long long int a,s,d,f;
+    cin>>a>>s>>d>>f;
+   long long int r=(a*s)-(d*f);
+    cout<<"Difference"<<" "<<"="<<" "<<r;
+    return 0;
+}
