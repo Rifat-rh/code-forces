95dda2a87d683599c1fa228742ec4030908a770d
Rifat-rh <129538138+Rifat-rh@users.noreply.github.com>
2023-06-09 23:04:56 +0600

    Add files via upload
    Say Hello with c++

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+/*  Say Hello with c++ :
+Given a name S. Print "Hello, (name)" without parentheses.
+
+Input
+Only one line containing a string S.
+
+Output
+Print "Hello, " without quotes, then print name.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char n[100];
+    cin>>n;
+    cout<<"Hello,"<<" "<<n;
+    return 0;
+}
