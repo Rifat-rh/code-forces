3117589422259dc95769bbc09b1b0c1051b13e40
Rifat-rh <129538138+Rifat-rh@users.noreply.github.com>
2023-06-21 21:46:31 +0600

    Update First_digit_!.cpp
    First digit

--- First_digit_!.cpp
+++ First_digit_!.cpp
@@ -1,4 +1,4 @@
-/* Find digit !
+/* First digit !
 Given a number X. Print "EVEN" if the first digit of X is even number. Otherwise print "ODD".
 
 For example: In 4569 the first digit is 4, the second digit is 5, the third digit is 6 and the fourth digit is 9.
