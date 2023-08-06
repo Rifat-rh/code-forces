18d190f33253fdfafc0e56032676b39e2f2d038c
MD.Rifat Hossain <129538138+Rifat-rh@users.noreply.github.com>
2023-08-07 00:48:09 +0600

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,39 @@
+/*
+You are given three integers a
+, b
+, and c
+. Determine if one of them is the sum of the other two.
+
+Input
+The first line contains a single integer t
+ (1≤t≤9261
+) — the number of test cases.
+
+The description of each test case consists of three integers a
+, b
+, c
+ (0≤a,b,c≤20
+).
+
+Output
+For each test case, output "YES" if one of the numbers is the sum of the other two, and "NO" otherwise.
+
+You can output the answer in any case (for example, the strings "yEs", "yes", "Yes" and "YES" will be recognized as a positive answer).
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+    while (n--)
+    {
+        int a,b,c;
+    cin>>a>>b>>c;
+    if(a+b==c || b+c==a || a+c==b){
+        cout<<"YES"<<endl;
+    }
+    else cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
